63d10f1ef8e7d952914d6e3a0fac1e8b16b8dc15
wsenodeb2026 <dmswmtnl25@gmail.com>
2026-09-19 13:09:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
+
 app.get("/", (req, res) => {
     res.send("WS Form Backend is running");
 });
